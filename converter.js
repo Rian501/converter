5d1bab8391facfff8d9120a6697be3661a8ca5e7
Rian501 <eclaire.lemmon@gmail.com>
2017-06-08 15:54:02 -0500

    Create ibput form; still need eventlisteners

--- converter.js
+++ converter.js
@@ -1,1 +1,12 @@
 console.log("At least the JS is linked.", );
+
+let userForm = document.getElementById("take-in-form");
+let userInput = document.getElementById("tempInput");
+
+
+
+userForm.addEventListener("submit", function() {
+
+
+
+})
